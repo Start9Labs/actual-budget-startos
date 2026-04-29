a5c758b28297492c6380c55621eec38e05b15a78
Aiden McClelland <me@drbonez.dev>
2026-04-28 23:06:24 -0600

    Bump version to 26.4.0:1

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,8 @@
 import { VersionGraph } from '@start9labs/start-sdk'
 import { v_26_4_0_0 } from './v26.4.0.0'
+import { v_26_4_0_1 } from './v26.4.0.1'
 
 export const versionGraph = VersionGraph.of({
-  current: v_26_4_0_0,
-  other: [],
+  current: v_26_4_0_1,
+  other: [v_26_4_0_0],
 })
