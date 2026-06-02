50e291c3ce68c2beb01f2cd65971b55f6ee85e3f
helix-nine <267227783+helix-nine@users.noreply.github.com>
2026-06-02 15:01:44 +0000

    chore: bump Actual Budget to 26.6.0; 26.5.2:0 → 26.6.0:0

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v_26_5_2_0 } from './v26.5.2.0'
+import { current } from './current'
 
 export const versionGraph = VersionGraph.of({
-  current: v_26_5_2_0,
+  current,
   other: [],
 })
